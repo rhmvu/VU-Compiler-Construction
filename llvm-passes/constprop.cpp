cdd78245a06ce86112a564d627cef0c54ecf1324
rubenvdham <ravdham@gmail.com>
2019-01-18 16:46:05 +0100

    Ass2 3: Started with constprop, need to access variable names now

--- llvm-passes/constprop.cpp
+++ llvm-passes/constprop.cpp
@@ -19,14 +19,44 @@ namespace {
 bool ConstPropPass::runOnFunction(Function &F) {
     bool modifiedCode = false;
 
+    SmallVector<ConstantFP*, 2> floatList;
+    SmallVector<ConstantInt*, 2> intList;
+    //Map for variable values we calculated/are constants
+
     for (BasicBlock &BB : F) {
         for (Instruction &II : BB) {
             Instruction *I = &II;
             if (BinaryOperator *BO = dyn_cast<BinaryOperator>(I)) {
                 LOG_LINE(" Binary op: " << *BO);
+                LOG_LINE(" Parent Name: " << I->getName());
+                for(Use &U : I->operands()){
+
+                    //check if ConstantInt
+                    if (ConstantInt *CI = dyn_cast<ConstantInt>(U)){
+                        LOG_LINE("     INT CONST: " << *CI);
+                        LOG_LINE("     NAME: " << CI->getName());
+                        intList.push_back(CI);
+                    }
+                    if (ConstantFP *CF = dyn_cast<ConstantFP>(U)){
+                        LOG_LINE("     FP CONST: " << *CF);
+                        LOG_LINE("     NAME: " << CF->getName());
+                        floatList.push_back(CF);
+                    }
+                    if (GlobalVariable *V = dyn_cast<GlobalVariable>(U)){
+                        LOG_LINE("     VAR: " << *V);
+                        LOG_LINE("     NAME: " << V->getName());
+                        //if variable check if it is in the map and it already has a value, if so put this value in int or float list. so we can calculate them later
+
+                    }
+                }
+                //if 2 operands and we can do calculation, then save the outcome in the variable map
             }
+
+
         }
     }
+
+
 
     return modifiedCode;
 }
