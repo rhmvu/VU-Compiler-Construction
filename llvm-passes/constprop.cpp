79f38d5a7457244d0b976980720a1c1f1211fb0a
rubenvdham <ravdham@gmail.com>
2019-01-20 17:48:24 +0100

    Ass 3 2: Progressed with integer only, nothing works yet

--- llvm-passes/constprop.cpp
+++ llvm-passes/constprop.cpp
@@ -19,10 +19,10 @@ namespace {
 bool ConstPropPass::runOnFunction(Function &F) {
     bool modifiedCode = false;
 
-    SmallVector<ConstantFP*, 2> floatList;
+//    SmallVector<ConstantFP*, 2> floatList;
     SmallVector<ConstantInt*, 2> intList;
-    std::map<std::string, Use*> variableMap;
-    std::map<std::string, Use*>::iterator variableMapIt;
+    std::map<std::string, IntConstant*> variableMap;
+    std::map<std::string, IntConstant*>::iterator variableMapIt;
     //Map for variable values we calculated/are constants
     std::string opCodeName;
 
@@ -32,67 +32,42 @@ bool ConstPropPass::runOnFunction(Function &F) {
             if (BinaryOperator *BO = dyn_cast<BinaryOperator>(I)) {
                 LOG_LINE("Binary op: " << *BO);
                 opCodeName = I->getOpcodeName();
-                        LOG_LINE("Parent Name: " << I->getName());
+                LOG_LINE("Parent Name: " << I->getName());
                 for(Use &U : I->operands()){
                     //check if ConstantInt
                     if (ConstantInt *CI = dyn_cast<ConstantInt>(U)){
                         LOG_LINE("     INT CONST: " << *CI);
                         intList.push_back(CI);
-                    }else if (ConstantFP *CF = dyn_cast<ConstantFP>(U)){
-                        LOG_LINE("     FP CONST: " << *CF);
-                        floatList.push_back(CF);
-                    } else{
+                    } else {
                         if (Instruction *V = dyn_cast<Instruction>(U)){
                             LOG_LINE("     Parent var: " << V->getName());
 
                             variableMapIt = variableMap.find(V->getName());
                             if(variableMapIt != variableMap.end()) {
-
-                                Use* varPointer = variableMapIt->second;
-                                if (ConstantInt *intVar = dyn_cast<ConstantInt>(varPointer)) {
-                                    intList.push_back(intVar);
-                                }else if(ConstantFP *floatVar = dyn_cast<ConstantFP>(varPointer)){
-                                    floatList.push_back(floatVar);
-                                }
+                                IntConstant* intVar = variableMapIt->second;
+                                intList.push_back(intVar);
                             }
                         }
 
                     }
                 }
                 //if 2 operands and we can do calculation, then save the outcome in the variable map
-                if (floatList.size() == 2){
-                    float result;
-                    switch(opCodeName){
-                        case "add": result = processfAdd(floatlist); break;
-                        case "mul": result = processfMul(floatlist); break;
-                        case "sub": result = processfSub(floatlist); break;
-                        //case "div": result = processfDiv(floatlist); break;
-                        case "ashr": result = processfshift(floatlist); break;
-                        default: LOG_LINE("ERRORROROROROROR, opcode unknown: "<< opCodeName); break;
-                    }
-
-                    /* get (Type *Ty, uint64_t V, bool isSigned=false) and, if you don't already have an integer type, IntegerType::get (LLVMContext &C, unsigned NumBits).*/
-                    ConstantFP* resultFP = new ConstantFP.get(FloatType.get(nullptr,64),result,true);
-                    ConstantFP *CF = dyn_cast<Use>(U)){
-
-                    variableMapIt->second = resultFP;
-
-
-                } else if(intList.size() == 2){
+                if(intList.size() == 2){
                     LOG_LINE(opCodeName);
 
-                    switch(opCodeName){
-                        case "add": result = processAdd(intlist); break;
-                        case "mul": result = processMul(intlist); break;
-                        case "sub": result = processSub(intlist); break;
-                        //case "div": result = processDiv(intlist); break;
-                        case "ashr": result = processshift(intlist); break;
-                        default: LOG_LINE("ERRORROROROROROR, opcode unknown: "<< opCodeName); break;
-                    }
+//                    switch(opCodeName){
+//                        case "add": result = processAdd(intlist); break;
+//                        case "mul": result = processMul(intlist); break;
+//                        case "sub": result = processSub(intlist); break;
+//                        //case "div": result = processDiv(intlist); break;
+//                        case "ashr": result = processshift(intlist); break;
+//                        default: LOG_LINE("ERRORROROROROROR, opcode unknown: "<< opCodeName); break;
+//                    }
 
-
-
-
+                    //ConstantInt* resultInt = ConstantInt.get(IntegerType.get(nullptr,64),result,true);
+//                    Use *resultUse = dyn_cast<Use>(resultFP);
+                    ConstantInt* resultInt = variableMapIt->second;
+                    APint = resultInt.getValue();
                     for(ConstantInt* ptr : intList){
                         LOG_LINE(*ptr);
                     }
