299b1eb6534ca030fe5913050546be19c13d2cb1
Ruben van der Ham <ravdham@protonmail.com>
2019-02-01 21:27:33 +0100

    Ass3: Changes so far, still doesn't compile

--- llvm-passes/BoundsCheck.cpp
+++ llvm-passes/BoundsCheck.cpp
@@ -15,64 +15,13 @@ class BoundsCheckerPass : public ModulePass
     //bool instrumentAllocations(Function &F);
     bool instrumentGetElementPointers(Function &F, Module &M);
     bool runOnFunction(Function &F);
-    Value* accumulatedOffset(GetElementPtrInst *gep, IRBuilder<> builder);
+    Value *accumulatedOffset(GetElementPtrInst *gep, IRBuilder<> builder);
     Value *getBasePointer(GetElementPtrInst *gep);
-    Value *getArraySize(Value* basePointer, Module &M);
+    Value *getArraySize(Value *basePointer, Module &M, IRBuilder<> builder);
     Value *PrepareCloneFunc(Argument *A, Module &M);
-    Value *cloneFunc(Argument *A, Module &M);
+    Value *cloneFunc(Argument *A, Module &M, IRBuilder<> builder);
 };
 } // namespace
-
-
-typedef struct funcClone{
-    Function* funcPtr;
-    SmallVector<Argument*, 2> newArgs();
-} funcClone_t;
-
-SmallVector<Function*, 2> workList;
-SmallVector<Type*, 2> typeList;
-SmallVector<Argument*, 2> argList;
-
-/*
- * Finds all allocations in a function and inserts a call that prints its size.
- */
-/*bool BoundsCheckerPass::instrumentAllocations(Function &F)
-{
-    bool Changed = false;
-
-    // We want to skip instrumenting certain functions, like declarations
-    // and helper functions (e.g., our dummy_print_allocation)
-    if (!shouldInstrument(&F))
-        return Changed;
-
-    // Construct an IRBuilder (at a random insertion point) so we can reuse it
-    // every time we need it.
-    IRBuilder<> builder(&F.getEntryBlock());
-
-    // Iterate over all instructions in this function.
-    for (Instruction &II : instructions(F))
-    {
-        Instruction *I = &II;
-
-        if (isa<AllocaInst>(I))
-        {
-            // GetElementPtrInst *G = dyn_cast<GetElementPtrInst>(I);
-            // LOG_LINE(*G);
-            // builder.SetInsertPoint(I);
-            // Value *offset = accumulatedOffset(G, builder);
-            // LOG_LINE("offsets = " << *offset);
-            // Value *arraySize = getArraySize(G);
-            // LOG_LINE("arraysize = " << *arraySize);
-        
-            // // CallInst *call = 
-            // builder.CreateCall(BoundsCheckHelper, {offset, arraySize});
-            // //builder.Insert(call);
-            // Changed = true;
-        }
-    }
-    return Changed;
-}
-*/
 
 bool BoundsCheckerPass::instrumentGetElementPointers(Function &F, Module &M)
 {
@@ -86,8 +35,8 @@ bool BoundsCheckerPass::instrumentGetElementPointers(Function &F, Module &M)
     // Construct an IRBuilder (at a random insertion point) so we can reuse it
     // every time we need it.
     IRBuilder<> builder(&F.getEntryBlock());
-    funcClone_t func;
-    func.funcPtr = &F;
+    //funcClone_t func;
+    //func.funcPtr = &F;
 
     // Iterate over all instructions in this function.
     for (Instruction &II : instructions(F))
@@ -103,15 +52,21 @@ bool BoundsCheckerPass::instrumentGetElementPointers(Function &F, Module &M)
             Value *offset = accumulatedOffset(G, builder);
             LOG_LINE("offsets = " << *offset);
             Value *basePointer = getBasePointer(G);
-            Value *arraySize = getArraySize(basePointer, M);
+            Value *arraySize;
             LOG_LINE("arraysize = " << *arraySize);
 
-            if (basePointer == arraySize) {
-               LOG_LINE("ARRAYSIZE ERROR, unhandled case");
+            if (basePointer == arraySize)
+            {
+                LOG_LINE("ARRAYSIZE ERROR, unhandled case");
             }
-            
-        
-            // CallInst *call = 
+
+            if (argumentAndSizeExist(G))
+            {
+                arraySize = getArraySize(basePointer, M, builder);
+                break;
+            }
+
+            // CallInst *call =
             builder.CreateCall(BoundsCheckHelper, {offset, arraySize});
             //builder.Insert(call);
             Changed = true;
@@ -120,6 +75,28 @@ bool BoundsCheckerPass::instrumentGetElementPointers(Function &F, Module &M)
     return Changed;
 }
 
+bool argumentAndSizeExist(GetElementPtrInst *G)
+{
+    if (Value *bp = dyn_cast<Argument>(G->getOperand(0)))
+    {
+        Function *newFunc = dyn_cast<Function>(G->getParent());
+
+        LOG_LINE("operand name: " << dyn_cast<Argument>(G->getOperand(0))->getName());
+        for (Function::arg_iterator AIT = newFunc->arg_begin(); AIT != newFunc->arg_end(); AIT++)
+        {
+            if (StringRef(AIT->getName()) == StringRef(bp->getName() + "cas"))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+    else
+    {
+        return false;cloneFu
+    }
+}
+/*
 Value *BoundsCheckerPass::PrepareCloneFunc(Argument *A, Module &M){
     //must be argumnet
     
@@ -133,7 +110,7 @@ Value *BoundsCheckerPass::PrepareCloneFunc(Argument *A, Module &M){
             int counter = 1;
             LOG_LINE("USER instruction" << *Ins);
             newArg = getArraySize(Ins, M);
-            for(Value *O :Ins->operands){
+            for(Value *O :Ins->operands()){
                 oldArgs.push_back(O);
                 argTypes.push_back(Int32Ty);
                 counter++;
@@ -149,68 +126,73 @@ Value *BoundsCheckerPass::PrepareCloneFunc(Argument *A, Module &M){
         }
     }
 
-}
+}*/
 
-Value *BoundsCheckerPass::cloneFunc(Argument *A, Module &M){
+Value *BoundsCheckerPass::cloneFunc(Argument *A, Module &M, IRBuilder<> builder)
+{
     //must be argumnet
-    
-    Type* Int32Ty = Type::getInt32Ty(M.getContext());
+
+    Type *Int32Ty = Type::getInt32Ty(M.getContext());
+    Type *Int32PtrTy = Type::getInt32PtrTy(M.getContext());
     Value *newArg;
-    SmallVector<Value*,2> oldArgs;
-    SmallVector<Type*,2> argTypes;
+    SmallVector<Value *, 2> oldArgs;
+    SmallVector<Type *, 2> argTypes;
     Function *parentFunction = A->getParent();
-    for(User *U : parentFunction->users()){
-        if(CallInst *Ins = dyn_cast<CallInst>(U)){
+    for (User *U : parentFunction->users())
+    {
+        if (CallInst *Ins = dyn_cast<CallInst>(U))
+        {
             int counter = 1;
             LOG_LINE("USER instruction" << *Ins);
-            newArg = getArraySize(Ins, M);
-            for(Value *O :Ins->operands){
+            newArg = getArraySize(Ins, M, builder);
+            for (Value *O : Ins->operands())
+            {
                 oldArgs.push_back(O);
                 argTypes.push_back(Int32Ty);
                 counter++;
             }
-            ArrayRef<Type*> newTypes = llvm::ArrayRef<Type*>(Int32Ty);
-            SmallVector<Argument*,2> newArgs;
-            Function *newFunc = addParamsToFunction(parentFunction,argTypes,newArgs);
+            ArrayRef<Type *> newParamTypes = llvm::ArrayRef<Type *>(Int32Ty);
+            SmallVector<Argument *, 8> newArgs;
+            Function *newFunc = addParamsToFunction(parentFunction, newParamTypes, newArgs);
             Argument *nca = newArgs.pop_back_val();
-            nca->setName(A->getName());
-            for(Function::arg_iterator AIT = newFunc->arg_begin(); AIT != newFunc->arg_end();AIT++){
-                if(StringRef(AIT->getName()) == StringRef("")){
+            nca->setName(A->getName() + "cas");
+            for (Function::arg_iterator AIT = newFunc->arg_begin(); AIT != newFunc->arg_end(); AIT++)
+            {
+                if (StringRef(AIT->getName()) == StringRef(""))
+                {
                     AIT->setName(nca->getName());
                 }
             }
-            //Function *cloneFunc = cast<Function>( M.getOrInsertFunction(newFunc->getName(), newFunc->getReturnType(), Int32Ty, Int32Ty));
-            
-            
-            //first should be ptr type
+            Function *cloneFunc = cast<Function>(M.getOrInsertFunction(newFunc->getName(), newFunc->getReturnType(), Int32PtrTy, Int32Ty));
 
+            //first should be ptr type
         }
     }
-
+    return nullptr; //later nca
 }
 
-
-
-Value *BoundsCheckerPass::getArraySize(Value *basePointer, Module &M)
+Value *BoundsCheckerPass::getArraySize(Value *basePointer, Module &M, IRBuilder<> builder)
 {
     if (AllocaInst *alloca = dyn_cast<AllocaInst>(basePointer))
     {
         return alloca->getArraySize();
     }
-    else if(Argument *A = dyn_cast<Argument>(basePointer)){
-        return PrepareCloneFunc(A, M);
-    } else
-    {   LOG_LINE("CONVERTING TO ARRAYTYPE");
+    else if (Argument *A = dyn_cast<Argument>(basePointer))
+    {
+        return cloneFunc(A, M, builder);
+    }
+    else
+    {
+        LOG_LINE("CONVERTING TO ARRAYTYPE");
         Type *ty = basePointer->getType();
         ArrayType *AT = dyn_cast<ArrayType>(ty->getPointerElementType());
-        if (AT == nullptr){
-            return basePointer; //Argument contains value at runtime  
+        if (AT == nullptr)
+        {
+            return basePointer; //Argument contains value at runtime
         }
         return ConstantInt::get(Type::getInt32Ty(ty->getContext()), AT->getNumElements(), true);
     }
 }
-
-
 
 // class funcClone
 // {
@@ -219,8 +201,7 @@ Value *BoundsCheckerPass::getArraySize(Value *basePointer, Module &M)
 //     SmallVector<Argument*, 2> newArgs();
 // };
 
-
-Value* BoundsCheckerPass::accumulatedOffset(GetElementPtrInst *gep, IRBuilder<> builder)
+Value *BoundsCheckerPass::accumulatedOffset(GetElementPtrInst *gep, IRBuilder<> builder)
 {
     // GetElementPtrInst *GEP = dyn_cast<GetElementPtrInst>(gep);
     GetElementPtrInst *operand1 = dyn_cast<GetElementPtrInst>(gep->getOperand(0));
@@ -249,14 +230,13 @@ Value *BoundsCheckerPass::getBasePointer(GetElementPtrInst *gep)
     }
 }
 
-
 //first add all pointertypes args in functions to worklist
 // for every arg in worklist with pointertype add count
-// for every count add arg in function, with the list tpye and argname starting 
+// for every count add arg in function, with the list tpye and argname starting
 // then add them to the function and loop through all USERS to add these arguments as wel.
 // then when these arguments are added loop to these users each as wel adding them recursively
 // start from allocas
-// 
+//
 //
 
 /*void insertArgumentInFunction(Function &F, Value* basePointer){
@@ -285,7 +265,8 @@ bool BoundsCheckerPass::runOnModule(Module &M)
     {
         //Changed |= instrumentAllocations(F);
         Changed |= instrumentGetElementPointers(F, M);
-        if(Changed){
+        if (Changed)
+        {
             //dosomething
         }
     }
